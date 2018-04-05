650302bd9e12a6133c3589cacabb60abfc2c30b9
Adrian Beer <adrianbeerka@gmail.com>
2018-04-05 22:14:31 +0200

    Add assertions in Driver class.

--- butterknife/src/Driver.py
+++ butterknife/src/Driver.py
@@ -34,7 +34,7 @@ class Driver:
     def current_website(self, value):
         self._current_website = value
         self.driver.get(value)
-    
+
     def get_links(self, source, class_name, amount):
         self.current_website = source
         previous_amount = -1
@@ -46,16 +46,19 @@ class Driver:
             self.scroll_down() #Scroll down to get access to more links
             links.extend(self.get_website_elements(By.CLASS_NAME, class_name, 'href'))
         return links[0:amount]
-    
+
     def get_website_elements(self, by, identifier, desired_attribute=None):
         elements = self._find_elements(by, identifier)
-        if desired_attribute is None:
-            return elements
         if desired_attribute == 'text':
-            return [e.text for e in elements]
+            elements =  [e.text for e in elements]
+        elif desired_attribute is None:
+            pass
         else:
-            return [e.get_attribute(desired_attribute) for e in elements]
-    
+            elements =  [e.get_attribute(desired_attribute) for e in elements]
+        assert elements is not None
+        assert len(elements) >= 1
+        return elements
+
     def scroll_down(self):
         self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
         sleep(uniform(1,2))
@@ -65,21 +68,21 @@ class Driver:
         if by == By.CLASS_NAME:
             elements = self.driver.find_elements_by_class_name(identifier)
         elif by == By.XPATH: 
-           elements = self.driver.find_elements_by_xpath(identifier)
+            elements = self.driver.find_elements_by_xpath(identifier)
         elif by == By.NAME:
-           elements = self.driver.find_elements_by_name(identifier)
+            elements = self.driver.find_elements_by_name(identifier)
         elif by == By.CSS_SELECTOR:
             elements = self.driver.find_elements_by_css_selector(identifier)
         elif by == By.ID:
-           elements = self.driver.find_elements_by_id(identifier)
+            elements = self.driver.find_elements_by_id(identifier)
         elif by == By.LINK_TEXT:
-           elements = self.driver.find_elements_by_link_text(identifier)
+            elements = self.driver.find_elements_by_link_text(identifier)
         elif by == By.PARTIAL_LINK_TEXT:
-           elements = self.driver.find_elements_by_partial_link_text(identifier)
+            elements = self.driver.find_elements_by_partial_link_text(identifier)
         elif by == By.TAG_NAME:
-           elements = self.driver.find_elements_by_tag_name(identifier)
+            elements = self.driver.find_elements_by_tag_name(identifier)
         else:
-            pass
+            raise NoSuchElementException
         return elements
 
     def __enter__(self):
@@ -90,5 +93,5 @@ class Driver:
 
     def quit(self):
         self.driver.quit()
-                                                      
-    
+
+
